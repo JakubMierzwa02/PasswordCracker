ed43f6bb35b6055fe2c2162d9431b024779ffaa4
JakubMierzwa02 <kontakt.jakubmierzwa@gmail.com>
2024-06-02 13:09:30 -0400

    Add documentation to several functions [#7]

--- src/HashAlgorithms.h
+++ src/HashAlgorithms.h
@@ -1,3 +1,7 @@
+/**
+ * @file HashAlgorithms.h
+*/
+
 #ifndef HASHALGORITHMS_H
 #define HASHALGORITHMS_H
 
@@ -6,9 +10,19 @@
 #include <cryptopp/hex.h>
 #include <cryptopp/filters.h>
 
+/**
+ * @class HashAlgorithms
+ * @brief Provides static methods for hashing strings using various algorithms
+*/
+
 class HashAlgorithms
 {
 public:
+    /**
+     * @brief Hashes an input string using SHA-256 and returns the hexadecimal representation of the hash
+     * @param input The input string to be hashed
+     * @return A string containing the hexadecimal representation of the SHA-256 hash of the input
+    */
     static std::string hashSHA256(const std::string& input)
     {
         CryptoPP::SHA256 hash;
--- src/PasswordCracker.h
+++ src/PasswordCracker.h
@@ -1,3 +1,6 @@
+/**
+ * @file PasswordCracker.h
+*/
 #ifndef PASSWORDCRACKER_H
 #define PASSWORDCRACKER_H
 
@@ -9,15 +12,38 @@
 #include <chrono>
 #include <fstream>
 #include "../test/googletest/googletest/include/gtest/gtest_prod.h"
-
 #include "HashAlgorithms.h"
 
+/**
+ * @class PasswordCracker
+ * @brief Implements methods to crack passwords using dictionary and brute-force attacks
+*/
 class PasswordCracker
 {
 public:
+    /**
+     * @brief Constructor for the PasswordCracker class
+    */
     PasswordCracker();
+
+    /**
+     * @brief Logs the results of the password cracking attempt to a file
+     * @param filename The name of the file to log the results to
+    */
     void logResults(const std::string& filename) const;
+
+    /**
+     * @brief Starts the password cracking process using the specified hash file and number of threads
+     * @param hashFile The file containing the hash to be cracked
+     * @param numThreads The number of threads to use for the cracking process
+    */
     void startCracking(const std::string& hashFile, int numThreads);
+
+    /**
+     * @brief Performs a dictionary attack using the specified dictionary file and number of threads
+     * @param dictionaryFile The file containing a list of potential passwords
+     * @param numThreads The number of threads to use for the dictionary attack
+    */
     void dictionaryAttack(const std::string& dictionaryFile, int numThreads);
     bool checkPassword(const std::string& password);
     bool isCracked() const;
